5faae1c371758905c4e71dca8a2f6cf35c21d0df
Ebubekir-Tas <tas_ebubekir@yahoo.com>
2024-10-01 16:59:04 -0400

    init

---
+++ rollup.config.mjs
@@ -0,0 +1,50 @@
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import postcss from 'rollup-plugin-postcss';
+import image from '@rollup/plugin-image';
+import url from '@rollup/plugin-url';
+import svgr from '@svgr/rollup';
+import pkg from './package.json' assert { type: 'json' };
+
+export default {
+  input: 'src/index.tsx', 
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: pkg.module,
+      format: 'esm',
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve({
+      browser: true,
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    }),
+    commonjs(),
+    typescript({
+      tsconfig: './tsconfig.json',
+      exclude: ['**/__tests__', '**/*.test.ts', '**/*.test.tsx'],
+    }),
+    postcss({
+      extensions: ['.css'],
+      extract: true,
+      minimize: true,
+      modules: true,
+    }),
+    image(),
+    url({
+      include: ['**/*.woff', '**/*.ttf', '**/*.eot'],
+      limit: 8192,
+    }),
+    svgr(),
+  ],
+  external: ['react', 'react-dom', 'react-konva'],
+};
