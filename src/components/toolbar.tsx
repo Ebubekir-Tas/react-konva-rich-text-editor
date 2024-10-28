9dd8783039cade10e48669d04e9f6474827258af
Ebubekir-Tas <tas_ebubekir@yahoo.com>
2024-10-28 18:52:51 -0400

    more fonts + toolbar style

--- src/components/toolbar.tsx
+++ src/components/toolbar.tsx
@@ -7,6 +7,7 @@ interface ToolbarProps {
   editor: Editor;
   options?: string[];
   setBubbleMenuElement: (element: HTMLElement) => void;
+  toolbarStyle?: React.CSSProperties;
 }
 
 const GlobalStyles = createGlobalStyle`
@@ -89,6 +90,7 @@ const Toolbar: React.FC<ToolbarProps> = ({
   editor,
   options = [],
   setBubbleMenuElement,
+  toolbarStyle,
 }) => {
   const toggleBold = useCallback(() => {
     editor.chain().focus().toggleBold().run();
@@ -183,10 +185,23 @@ const Toolbar: React.FC<ToolbarProps> = ({
         className="bubble-menu-select"
         onChange={(e) => toggleFont(e.target.value)}
       >
-        <option value="Arial">Arial</option>
         <option value="Times New Roman">Times New Roman</option>
+        <option value="Arial">Arial</option>
+        <option value="Helvetica">Helvetica</option>
+        <option value="Times">Times</option>
         <option value="Courier New">Courier New</option>
+        <option value="Courier">Courier</option>
+        <option value="Verdana">Verdana</option>
+        <option value="Georgia">Georgia</option>
+        <option value="Palatino">Palatino</option>
+        <option value="Garamond">Garamond</option>
+        <option value="Bookman">Bookman</option>
         <option value="Comic Sans MS">Comic Sans MS</option>
+        <option value="Trebuchet MS">Trebuchet MS</option>
+        <option value="Arial Black">Arial Black</option>
+        <option value="Impact">Impact</option>
+        <option value="Lucida Sans Unicode">Lucida Sans Unicode</option>
+        <option value="Tahoma">Tahoma</option>
       </select>
     ),
     color: (
@@ -214,7 +229,7 @@ const Toolbar: React.FC<ToolbarProps> = ({
           },
         }}
       >
-        <div className="bubble-menu-container">
+        <div className="bubble-menu-container" style={toolbarStyle}>
           {options.map((option) => {
             const button = buttonComponents[option];
             if (!button) {
