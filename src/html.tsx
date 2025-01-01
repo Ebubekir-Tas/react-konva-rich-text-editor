e51865567e8045e52754eb8d337e51d5e8feef21
ebubekir-tas <tas_ebubekir@yahoo.com>
2024-12-31 21:24:41 -0500

    html component rendering logic

--- src/html.tsx
+++ src/html.tsx
@@ -2,38 +2,44 @@ import { useEffect, useRef } from 'react';
 import { createRoot, Root } from 'react-dom/client';
 
 interface HtmlProps {
-	children: React.ReactNode;
-	container: Element | null;
-};
+  children: React.ReactNode;
+  container: Element | null;
+}
 
 export const Html: React.FC<HtmlProps> = ({ children, container }) => {
-	const containerRef = useRef<HTMLDivElement>(document.createElement("div"));
-	const rootRef = useRef<Root | null>(null);
+  const containerRef = useRef<HTMLDivElement>(document.createElement('div'));
+  const rootRef = useRef<Root | null>(null);
 
-	useEffect(() => {
-		if (!container) return;
+  // Mounting and unmounting logic
+  useEffect(() => {
+    if (!container) return;
 
-		if (!container.contains(containerRef.current)) {
-			container.appendChild(containerRef.current);
-		}
+    if (!container.contains(containerRef.current)) {
+      container.appendChild(containerRef.current);
+    }
 
-		if (!rootRef.current) {
-			rootRef.current = createRoot(containerRef.current);
-		}
+    if (!rootRef.current) {
+      rootRef.current = createRoot(containerRef.current);
+    }
 
-		rootRef.current.render(<>{children}</>);
+    return () => {
+      if (rootRef.current) {
+        rootRef.current.unmount();
+        rootRef.current = null;
+      }
 
-		return () => {
-			if (rootRef.current) {
-				rootRef.current.unmount();
-				rootRef.current = null;
-			}
+      if (containerRef.current && container.contains(containerRef.current)) {
+        container.removeChild(containerRef.current);
+      }
+    };
+  }, [container]);
 
-			if (containerRef.current && container.contains(containerRef.current)) {
-				container.removeChild(containerRef.current);
-			}
-		}
-	}, [container]);
+  // Rendering children
+  useEffect(() => {
+    if (!rootRef.current) return;
 
-	return null;
+    rootRef.current.render(<>{children}</>);
+  }, [children]);
+
+  return null;
 };
